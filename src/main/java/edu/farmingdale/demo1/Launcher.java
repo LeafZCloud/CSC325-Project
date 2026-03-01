c30b27f4e6d25f3c174397d465f74a5cc3b99435
Hamdi Tekci <hamdit2006@outlook.com>
2026-02-28 20:08:08 -0500

    added the firebase dependency to the pom file, also put it in gitignore so that the private key cannot be accessed from the GitHub

--- src/main/java/edu/farmingdale/demo1/Launcher.java
+++ src/main/java/edu/farmingdale/demo1/Launcher.java
@@ -1,9 +1,16 @@
 package edu.farmingdale.demo1;
 
+import com.google.firebase.auth.FirebaseAuth;
 import javafx.application.Application;
+
+import java.io.*;
 
 public class Launcher {
     public static void main(String[] args) {
         Application.launch(HelloApplication.class, args);
+        FirebaseAuth.getInstance();
+
+            InputStream inputStream = Launcher.class.getResourceAsStream("/classproject-1717-firebase-adminsdk-fbsvc-4ed10da603.json");
+
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,6 +2,7 @@ module edu.farmingdale.demo1 {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.desktop;
+    requires firebase.admin;
 
 
     opens edu.farmingdale.demo1 to javafx.fxml;
