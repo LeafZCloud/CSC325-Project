b4d0abc1a6894d00e38c37751e402d81c5d04844
Brandon Douglass <brandonjdouglass@yahoo.com>
2026-03-01 19:29:34 -0500

    Learned how to add image to login / start screen background. Added new game / create account buttons but they currently have no functionality.

--- src/main/java/edu/farmingdale/demo1/GameController.java
+++ src/main/java/edu/farmingdale/demo1/GameController.java
@@ -1,38 +1,53 @@
 package edu.farmingdale.demo1;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.util.Duration;
 import javafx.scene.shape.Circle;
+import javafx.stage.Stage;
+
 import java.util.Random;
 
 public class GameController {
 
     @FXML
+    private StackPane rootPane;
+
+    @FXML
     private Pane gamePane;
 
     private Random random = new Random();
-    private Polygon northAmerica;  // Keep a reference
+    private Polygon northAmerica;
 
     @FXML
     public void initialize() {
+
+        // Make gamePane resize with window (fullscreen safe)
+        gamePane.prefWidthProperty().bind(rootPane.widthProperty());
+        gamePane.prefHeightProperty().bind(rootPane.heightProperty());
+
         drawNorthAmerica();
     }
 
     private void drawNorthAmerica() {
+
         northAmerica = new Polygon(
-                200, 100, // upper-left
-                300, 80,  // top
-                400, 120, // upper-right
-                450, 200, // middle-right
-                350, 300, // bottom-right
-                200, 250, // bottom-left
-                180, 150  // middle-left
+                200, 100,
+                300, 80,
+                400, 120,
+                450, 200,
+                350, 300,
+                200, 250,
+                180, 150
         );
+
         northAmerica.setFill(Color.GREEN);
         northAmerica.setStroke(Color.DARKGREEN);
         northAmerica.setStrokeWidth(2);
@@ -42,24 +57,47 @@ public class GameController {
 
     @FXML
     private void handleSnowEffect() {
-        // Turn the continent white
+
         if (northAmerica != null) {
             northAmerica.setFill(Color.WHITE);
         }
 
-        // Create snow animation
         Timeline timeline = new Timeline(new KeyFrame(Duration.millis(200), event -> {
-            Circle snowflake = new Circle(random.nextInt((int) gamePane.getWidth()), 0, 5, Color.WHITE);
+
+            Circle snowflake = new Circle(
+                    random.nextInt((int) gamePane.getWidth()),
+                    0,
+                    5,
+                    Color.WHITE
+            );
+
             gamePane.getChildren().add(snowflake);
 
-            // Falling animation
-            Timeline fall = new Timeline(new KeyFrame(Duration.millis(50), e -> {
-                snowflake.setLayoutY(snowflake.getLayoutY() + 5);
-            }));
+            Timeline fall = new Timeline(new KeyFrame(Duration.millis(50), e ->
+                    snowflake.setLayoutY(snowflake.getLayoutY() + 5)
+            ));
+
             fall.setCycleCount(100);
             fall.play();
+
         }));
+
         timeline.setCycleCount(50);
         timeline.play();
+    }
+
+    @FXML
+    private void handleBackToStart() {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/start.fxml"));
+            Scene scene = new Scene(loader.load());
+
+            Stage stage = (Stage) rootPane.getScene().getWindow();
+            stage.setScene(scene);
+            stage.setFullScreen(true);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
