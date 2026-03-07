6759a5b1c3b91da71dd17276817b0940e4c68330
Hamdi Tekci <hamdit2006@outlook.com>
2026-03-06 19:15:24 -0500

    completed the login method and its request functionality, it can now send POST request to the firebase DB using the user email and password and return a boolean if the account is found or not.

--- src/main/java/FirebaseAuthService.java
+++ src/main/java/FirebaseAuthService.java
@@ -1,6 +1,10 @@
 import com.google.gson.Gson;
 import com.google.gson.JsonObject;
+import okhttp3.Request;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
+import okhttp3.RequestBody;
+import okhttp3.Response;
 
 public class FirebaseAuthService
 {
@@ -19,6 +23,9 @@ public class FirebaseAuthService
     //Instantiation fo the Gson field
     private final Gson gson = new Gson();
 
+    //Media type is to tell the server what kind of data you are sending (application/json)
+    public static final MediaType JSON = MediaType.get("application/json");
+
 
     //Login, POST request
     public boolean login(String email, String password){
@@ -30,7 +37,28 @@ public class FirebaseAuthService
         body.addProperty("password", password); //password for the account
         body.addProperty("returnSecureToken", true);
 
+        //This instantiates the RequestBody, with the parameters of the JSON body in string format and the type of data that you are sending it as (JSON)
+        RequestBody requestBody = RequestBody.create(body.toString(),JSON);
 
-        return true;
+        //This makes the request to the URL and gives it the already instantiated requestBody
+        Request request = new Request.Builder().url(SIGN_IN_URL).post(requestBody).build();
+
+        //This tries with resources and executes the request
+        try(Response response = client.newCall(request).execute()) {
+            //Converts the response body into the JsonObject by parsing it into a string using the .gson method
+            JsonObject json = gson.fromJson(response.body().string(), JsonObject.class);
+
+            //Checks to see if the json request contains an "idToken" in the json object
+            if(json.has("idToken")) {
+                return true;
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
+
+        return false;
     }
+
 }
