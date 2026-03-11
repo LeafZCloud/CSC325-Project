65610396ab46ce19cff3343c7eb74df57e3c23af
Alif Z <Zamaa@farmingdale.edu>
2026-03-11 11:33:37 -0400

    Set start screen as default launch scene

--- src/main/java/edu/farmingdale/demo1/HelloApplication.java
+++ src/main/java/edu/farmingdale/demo1/HelloApplication.java
@@ -9,9 +9,9 @@ public class HelloApplication extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/login.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/start.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 600, 400);
-        stage.setTitle("PlanetSim Login");
+        stage.setTitle("Planet Sim");
         stage.setScene(scene);
         stage.show();
     }
