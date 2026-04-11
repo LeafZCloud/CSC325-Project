55c8a23b81fec66bc5a96fb997d48818329e1a7b
Hamdi Tekci <hamdit2006@outlook.com>
2026-04-10 21:04:20 -0400

    Refactored the DatabaseController, Created the saveGameState method

--- src/main/java/edu/farmingdale/demo1/Database/DatabaseController.java
+++ src/main/java/edu/farmingdale/demo1/Database/DatabaseController.java
@@ -1,15 +1,15 @@
 package edu.farmingdale.demo1.Database;
 
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import edu.farmingdale.demo1.simulation.GameTypes;
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import java.util.Map;
-import java.util.HashMap;
+import okhttp3.*;
+import java.io.IOException;
+import java.util.*;
 
 public class DatabaseController extends GameTypes
 {
-
+    //Tool initialization
     private final OkHttpClient client = new OkHttpClient();
     private final Gson gson = new Gson();
     public static final MediaType JSON = MediaType.get("application/json");
@@ -17,28 +17,39 @@ public class DatabaseController extends GameTypes
     //Firestore rest url
     private static final String FIRESTORE_URL = "https://firestore.googleapis.com/v1/projects/classproject-1717/databases/(default)/documents/gameStates/";
 
+    //This methods will save the users game state when the trigger of either Exiting the game, Closing the game
+    //or using the save button
+    public boolean saveGameState(GameState gameState, String userId, String idToken) {
 
-    public static Map<String, Object> gameStateObject(GameState gameState) {
-        Map<String, Object> gameStateMap = new HashMap<>();
+        String gameStateJson = gson.toJson(gameState);
+        JsonObject stringValue = new JsonObject();
+        stringValue.addProperty("stringValue", gameStateJson);
 
-        //This is the map that gets sent to the firestore for the instance
-        gameStateMap.put("cooldowns", gameState.cooldowns);
-        gameStateMap.put("feedPosts", gameState.feedPosts);
-        gameStateMap.put("eventLog", gameState.eventLog);
-        gameStateMap.put("globalStats", gameState.globalStats);
-        gameStateMap.put("year", gameState.year);
-        gameStateMap.put("flashingRegions", gameState.flashingRegions);
-        gameStateMap.put("lastEventId", gameState.lastEventId);
-        gameStateMap.put("regions", gameState.regions);
-        gameStateMap.put("planet", gameState.planet);
+        JsonObject fields = new JsonObject();
+        fields.add("data", stringValue);
 
-        return gameStateMap;
+        JsonObject body = new JsonObject();
+        body.add("fields", fields);
+
+        RequestBody requestBody = RequestBody.create(body.toString(), JSON);
+        Request request = new Request.Builder()
+                .url(FIRESTORE_URL + userId)
+                .patch(requestBody)
+                .addHeader("Authorization", "Bearer " + idToken)
+                .build();
+
+        try (Response response = client.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                System.out.println("Game state saved for user: " + userId);
+                return true;
+            } else {
+                System.out.println("Save failed. Code: " + response.code()
+                        + " Body: " + response.body().string());
+                return false;
+            }
+        } catch (IOException e) {
+            System.out.println("Save error: " + e.getMessage());
+            return false;
+        }
     }
-
-    //uses the users authentication token to return true or false if it works or not
-    public boolean saveGameState(GameState gameState, String idToken) {
-        Map<String, Object> data = gameStateObject(gameState);
-        return false;
-    }
-
 }
